d6c9b78ca78fd87366eb2419e10a00403a65b5e9
jzy <jiaozhiye@163.com>
2022-03-13 09:48:41 +0800

    1234

--- build/build-style.gulp.js
+++ build/build-style.gulp.js
@@ -2,19 +2,21 @@
  * @Author: 焦质晔
  * @Date: 2021-02-11 15:05:17
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-10-10 10:36:33
+ * @Last Modified time: 2022-03-13 09:43:02
  */
 'use strict';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { series, src, dest } = require('gulp');
-const sass = require('gulp-dart-sass');
+const gulpSass = require('gulp-sass');
+const dartSass = require('sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const utils = require('./utils');
 
 function compile() {
+  const sass = gulpSass(dartSass);
   return src(utils.resolve('packages/style/src/index.scss'))
     .pipe(sass.sync({ includePaths: [utils.resolve('node_modules')] }))
     .pipe(autoprefixer({ cascade: false }))
